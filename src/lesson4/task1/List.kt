26ea439d5146cd1a01ca42057041647a3ea6d143
Fusion <lethal@mail.ua>
2021-01-17 22:39:21 +0200

    Реализована функция convert для перевода целого числа из 10-ичной в N-ичную систему счисления. Алгоритм реализован с помощью деления нацело и остатка от деления на основание.

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -225,7 +225,15 @@ fun factorizeToString(n: Int): String = factorize(n).joinToString(separator = "*
  * Результат перевода вернуть в виде списка цифр в base-ичной системе от старшей к младшей,
  * например: n = 100, base = 4 -> (1, 2, 1, 0) или n = 250, base = 14 -> (1, 3, 12)
  */
-fun convert(n: Int, base: Int): List<Int> = TODO()
+fun convert(n: Int, base: Int): List<Int> {
+    var mutN = n
+    val digitsList = mutableListOf<Int>()
+    while (mutN > 0) {
+        digitsList.add(0, mutN % base)
+        mutN /= base
+    }
+    return digitsList
+}
 
 /**
  * Сложная (4 балла)
