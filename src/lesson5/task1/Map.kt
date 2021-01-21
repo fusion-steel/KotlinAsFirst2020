ea48ec6ed342ce515975b6a11b5ee5251d3968c3
Fusion <lethal@mail.ua>
2021-01-21 20:36:56 +0200

    Реализована функция canBuildFrom путем проверки вхождения множества word.toSet() в множество chars.

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -198,7 +198,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean = TODO()
+fun canBuildFrom(chars: List<Char>, word: String): Boolean = chars.containsAll(word.toSet())
 
 /**
  * Средняя (4 балла)
